4090e806a1ef3b58455d4beafb3ea1fa31d4d550
rheredia <totti.heredia@gmail.com>
2018-11-27 12:19:45 -0300

    Config app for heroku

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -224,4 +224,5 @@ return [
 
     ],
 
+    'log' => 'errorlog'
 ];
