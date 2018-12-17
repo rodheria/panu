5c317fed0ec090f95b9182b492b0337be29074d1
rheredia <totti.heredia@gmail.com>
2018-12-17 14:16:06 -0300

    Faq con foto, falta foto y apellido geo

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,28 +14,16 @@
 			<h2 class="my-4">Nuestro equipo</h2>
 		  </div>
 		  <div class="col-lg-4 col-sm-6 text-center mb-4">
-			<img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-			<h3>Gustavo Bargas</h3>
-			<h4>Job Title</h4>
-			<p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-		  </div>
-		  <div class="col-lg-4 col-sm-6 text-center mb-4">
-			<img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+            <img class="rounded-circle img-fluid d-block mx-auto" src="{{asset('images/about/rodrigoheredia.jpg')}}" width="200" alt="">
 			<h3>Rodrigo Heredia</h3>
-			<h4>Job Title</h4>
-			<p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-		  </div>
-		  <div class="col-lg-4 col-sm-6 text-center mb-4">
-			<img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-			<h3>Pablo O'Connor</h3>
-			<h4>Job Title</h4>
-			<p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+			<h4>Programador</h4>
+			<p><a href="https://github.com/rodheria/" target="_blank">Github</a></p>
 		  </div>
 		  <div class="col-lg-4 col-sm-6 text-center mb-4 mx-auto">
 			<img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
 			<h3>Georgina Ramp</h3>
-			<h4>Job Title</h4>
-			<p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+			<h4>Programadora</h4>
+			<p><a href="https://github.com/georamp/" target="_blank">Github</a></p>
 		  </div>
 		</div>
 
