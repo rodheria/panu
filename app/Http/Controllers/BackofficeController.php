70a4059221edfbc7f9cccef7e14d0d0d90c4ffe1
rheredia <totti.heredia@gmail.com>
2018-12-18 19:04:41 -0300

    Mejoras en el backoffice (No funcional)

--- app/Http/Controllers/BackofficeController.php
+++ app/Http/Controllers/BackofficeController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Post;
 
 class BackofficeController extends Controller
 {
     public function index()
     {
         $users = User::all();
-        return view('backoffice.index')->with('users', $users);
+        $posts = Post::all();
+        return view('backoffice.index')->with('users', $users)->with('posts', $posts);
     }
 
     public function create_category()
@@ -29,6 +31,13 @@ class BackofficeController extends Controller
     public function user_delete($id){
         $user = User::findOrFail($id);
         $user->destroy();
+
+        return Redirect::route()->back();
+    }
+
+    public function post_delete($id){
+        $post = Post::findOrFail($id);
+        $post->destroy();
 
         return Redirect::route()->back();
     }
--- resources/views/backoffice/index.blade.php
+++ resources/views/backoffice/index.blade.php
@@ -7,22 +7,50 @@
         <h4>Usuarios activos</h4>
         <hr>
         <ul class="unstyled-list">
-            @foreach($users as $user)
-            <li class="d-flex flex-row">
-                <a class="mr-auto p-2" href="/user/{{ $user->id }}">{{ $user->name }}</a>
-                <div class="buttons-admin d-flex">
-                    <form action="/backoffice/user/delete/{{ $user->id }}" method="post" class="p-2">
-                        {{ csrf_field() }}
-                        {{ method_field('delete') }}
-                        <input type="submit" value="Eliminar" class="btn btn-danger">
-                    </form>
-                    <div class="d-flex">
-                        <a href="#" class="btn btn-warning p-2">Editar</a>
-                    </div>  
-                </div>
-            </li>
-            <hr>
-            @endforeach
+            <button data-toggle="collapse" data-target="#users">Usuarios</button>
+            <div id="users" class="collapse">
+                @foreach($users as $user)
+                <li class="d-flex flex-row">
+                    <a class="mr-auto p-2" href="/user/{{ $user->id }}">{{ $user->name }}</a>
+                    <div class="buttons-admin d-flex">
+                        <form action="/backoffice/user/delete/{{ $user->id }}" method="post" class="p-2">
+                            {{ csrf_field() }}
+                            {{ method_field('delete') }}
+                            <input type="submit" value="Eliminar" class="btn btn-danger">
+                        </form>
+                        <div class="d-flex">
+                            <a href="#" class="btn btn-warning p-2">Editar</a>
+                        </div>  
+                    </div>
+                </li>
+                <hr>
+                @endforeach
+            </div>
+            
+        </ul>
+        <h4>Posts activos</h4>
+        <hr>
+        <ul class="unstyled-list">
+            <button data-toggle="collapse" data-target="#posts">Posts</button>
+            <div id="posts" class="collapse">
+                @foreach($posts as $post)
+                <li class="d-flex flex-row">
+                    <a class="mr-auto p-2" href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    <div class="buttons-admin d-flex">
+                        <form action="/backoffice/user/delete/{{ $post->id }}" method="post" class="p-2">
+                            {{ csrf_field() }}
+                            {{ method_field('delete') }}
+                            <input type="submit" value="Eliminar" class="btn btn-danger">
+                        </form>
+                        <div class="d-flex">
+                            <a href="#" class="btn btn-warning p-2">Editar</a>
+                        </div>  
+                    </div>
+                </li>
+                <hr>
+                @endforeach
+            </div>
+            
         </ul>
     </div>
 </div>
